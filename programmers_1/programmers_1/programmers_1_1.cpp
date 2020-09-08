9fbeddf1c020fd0a06202d82674cf537cde81444
Singga <qwerewqaa@naver.com>
2020-09-08 17:26:43 +0900

    modified code

--- programmers_1/programmers_1/programmers_1_1.cpp
+++ programmers_1/programmers_1/programmers_1_1.cpp
@@ -1,17 +1,15 @@
-// 문제 1) 정수 n을 입력받아 n의 약수를 모두 더한 값을 return 하는 함수를 완성해주세요.
+// 문제 1) 정수 n을 입력받아 n의 약수를 모두 더한 값을 return 하는 solution 함수를 완성해주세요.
 // n은 0이상 3000이하인 정수입니다.
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-void main()
-{
-	int n;
-	int answer = 0;
+int n;
 
-	printf("정수 입력 : ");
-	scanf_s("%d", &n);
+int solution()
+{
+	int answer = 0;
 
 	for (int i = 1; i <= n; i++)
 	{
@@ -20,5 +18,14 @@ void main()
 			answer += i;
 		}
 	}
-	printf("%d", answer);
+	return answer;
+}
+
+void main()
+{
+	printf("정수 입력 : ");
+	scanf_s("%d", &n);
+
+	int sol = solution();
+	printf("%d", sol);
 }
---
+++ programmers_1/programmers_1_2/programmers_1_2.cpp
@@ -0,0 +1,11 @@
+// 문제 2) 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수를 완성하시오
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void main()
+{
+	int n;
+
+}
