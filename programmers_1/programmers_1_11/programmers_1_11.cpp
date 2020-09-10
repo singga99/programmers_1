3e84875724d64424284bcdc0759dee6cfd1417e9
Singga <qwerewqaa@naver.com>
2020-09-10 20:06:07 +0900

    Level_1_11

---
+++ programmers_1/programmers_1_11/programmers_1_11.cpp
@@ -0,0 +1,16 @@
+// 문제 11번) 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수를 완성하시오.
+// ex) n이 4이면 "수박수박"을 리턴, 3이면 "수박수" 리턴
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	int n;
+
+	printf("문자 길이를 적어주시오 : ");
+	scanf_s("%d", &n);
+
+}
